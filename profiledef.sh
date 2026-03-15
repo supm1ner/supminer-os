05d049ac9b572f6e7e146f82421a73c248e55073
supm1ner <supm1ner@users.noreply.github.com>
2026-03-15 13:42:04 +0300

    fix: fix mkarchiso boot modes and add syslinux/grub configs
    - Replace deprecated boot modes with bios.syslinux, uefi-x64.grub, uefi-x64.systemd-boot
    - Add syslinux/syslinux.cfg for BIOS boot
    - Add grub/grub.cfg with B&W color scheme
    - Add syslinux to packages.x86_64

--- profiledef.sh
+++ profiledef.sh
@@ -8,9 +8,7 @@ iso_application="SupMiner OS Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux' 'uefi-x64.grub' 'uefi-x64.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
